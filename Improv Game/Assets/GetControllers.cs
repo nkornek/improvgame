04abe6784d373cd885c9ecbafc4343f8ce106abe
Nick Kornek <nkornek@gmail.com>
2014-12-04 13:16:41 -0500

    controller scripts

---
+++ Improv Game/Assets/GetControllers.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+using UnityEngine.UI;
+
+public class GetControllers : MonoBehaviour {
+
+    public bool[] controllerConnected;
+    GamePadState state;
+    GamePadState prevState;
+    public bool[] playerInGame;
+
+
+    //display disconnection
+    public Image[] disconnection;
+    public Text disconnectText;
+    public Image[] disconnectUI;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //hide disconnection UI
+        disconnectUI[0].enabled = false;
+        disconnectUI[1].enabled = false;
+        disconnectText.enabled = false;
+        foreach (Image image in disconnection)
+        {
+            image.enabled = false;
+        }
+        
+        for (int i = 0; i < 4; ++i)
+        {
+            PlayerIndex testPlayerIndex = (PlayerIndex)i;
+            GamePadState testState = GamePad.GetState(testPlayerIndex);
+            controllerConnected[i] = testState.IsConnected;
+            prevState = state;
+            state = GamePad.GetState((PlayerIndex)i);
+
+            if (controllerConnected[i])
+            {
+                if (prevState.Buttons.Start == ButtonState.Released && state.Buttons.Start == ButtonState.Pressed)
+                {
+                    playerInGame[i] = true;
+                }
+ 
+            }
+            
+            if (playerInGame[i] & !controllerConnected[i])
+            {
+                print("Reconnect: " + (i + 1));
+                //change for a visible display later
+                disconnectUI[0].enabled = true;
+                disconnectUI[1].enabled = true;
+                disconnectText.enabled = true;
+                disconnection[i].enabled = true;
+            }
+        }
+
+
+
+	
+	}
+}
---
+++ Improv Game/Assets/Scripts/WriterControls.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class WriterControls : MonoBehaviour {
+    public GetControllers controllerMaster;
+
+	PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            choosePlayer();
+        }
+	
+	}
+	
+	public void choosePlayer()
+	{
+        
+		int whichPlayer = 0;
+		playerIndex = (PlayerIndex)whichPlayer;
+        GamePad.SetVibration(playerIndex, 1, 1);
+        Invoke("StopRumble", 0.3f);
+		
+	}
+
+	public void playerControls()
+	{
+		
+	}
+
+    void StopRumble()
+    {
+        GamePad.SetVibration(playerIndex, 0, 0);
+    }
+
+}
