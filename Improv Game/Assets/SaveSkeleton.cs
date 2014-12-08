d12fcc3d9102fd6e19199c7f52586449e9b09124
nkornek <nkornek@gmail.com>
2014-12-08 16:57:28 -0500

    character cloning

--- Improv Game/Assets/SaveSkeleton.cs
+++ Improv Game/Assets/SaveSkeleton.cs
@@ -1,7 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 public class SaveSkeleton : MonoBehaviour {
+    public ChoiceText choiceScript;
+    public Transform closet;
+
+    public int act, scene, player;
+    public bool canClone;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +18,28 @@ public class SaveSkeleton : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        //debug
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CloneSkeleton();
+        }       	
 	}
+
+    void CloneSkeleton()
+    {
+        if (canClone)
+        {
+            act = choiceScript.actNum;
+            scene = choiceScript.sceneNum;
+            GameObject SkeletonClone;
+            SkeletonClone = Instantiate(gameObject) as GameObject;
+            SkeletonClone.GetComponent<SaveSkeleton>().canClone = false;
+            SkeletonClone.transform.parent = closet;
+        }
+    }
+
+
+
+
 }
