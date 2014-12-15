03aa3430975fb30708cbfc1a80d02b9c7a8bbb46
Louis <louis.sciannamblo@gmail.com>
2014-12-15 12:30:46 -0500

    TITLE

---
+++ Improv Game/Assets/STARTTHISGAMEGUY.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class STARTTHISGAMEGUY : MonoBehaviour {
+
+	public void TheTimeHasCome ()
+	{
+		Application.LoadLevel (1);
+		}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
