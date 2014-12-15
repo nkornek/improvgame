f84dd25b6b84ea2f75b2eb01a713cf1b9fa8d3bb
nkornek <nkornek@gmail.com>
2014-12-14 21:00:44 -0500

    more integration

--- Improv Game/Assets/PropScript.cs
+++ Improv Game/Assets/PropScript.cs
@@ -3,31 +3,14 @@ using System.Collections;
 
 public class PropScript : MonoBehaviour {
 
-    public enum PropTypes {held, background, foreground}
+    public enum PropTypes {held, background, foreground, clothesHat}
     public PropTypes propType;
 
     public int playerNum;
 
 	// Use this for initialization
 	void Start () {
-        switch (propType)
-        {
-            case PropTypes.held:
-                foreach (GameObject g in GameObject.FindGameObjectsWithTag("Player"))
-                {
-                    if (g.GetComponent<SaveSkeleton>().player == playerNum)
-                    {
-                        transform.parent = g.GetComponent<SaveSkeleton>().handAttach.transform;
-                        transform.position = transform.parent.position;
-                        transform.rotation = transform.parent.rotation;
-                    }
-                }
-                break;
-            case PropTypes.background:
-                break;
-            case PropTypes.foreground:
-                break;
-        }
+       
 
 	
 	}
@@ -35,5 +18,29 @@ public class PropScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void SetProp()
+	{
+		switch (propType)
+		{
+		case PropTypes.held:
+			foreach (GameObject g in GameObject.FindGameObjectsWithTag("Player"))
+			{
+				if (g.GetComponent<SaveSkeleton>().player == playerNum)
+				{
+					transform.parent = g.GetComponent<SaveSkeleton>().handAttach.transform;
+					transform.position = transform.parent.position;
+					transform.rotation = transform.parent.rotation;
+				}
+			}
+			break;
+		case PropTypes.background:
+			transform.parent = GameObject.Find("Background Parent").transform;
+			transform.position = transform.parent.position;
+			break;
+		case PropTypes.foreground:
+			break;
+		}
 	}
 }
