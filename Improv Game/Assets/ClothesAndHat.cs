38a5dc5f123fe2bfb0bad64909fe61699b11317b
nkornek <nkornek@gmail.com>
2014-12-14 23:14:59 -0500

    whole lot more stuff

---
+++ Improv Game/Assets/ClothesAndHat.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClothesAndHat : MonoBehaviour {
+	public GameObject hat, clothes;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
